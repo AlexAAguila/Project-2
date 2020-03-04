55e7f4d53ec4911702d7db09c7ef8275e2a3fcaf
Alex <alex_a_aguilar@outlook.com>
2020-03-03 21:49:22 -0800

    Nav change coulers

--- js/scroll.js
+++ js/scroll.js
@@ -4,17 +4,18 @@ console.log('JavaScript is Running. YAAAAAY');
 
 const htmlTag= document.querySelector('html');
 const bodyTag = document.querySelector('body');
-const myNav = document.querySelector('.row-100-footer');
+const myNav = document.querySelector('header');
+const myElem = document.querySelector('.row-100-yellow');
 
 
 let scrolled = () => {
     let dec = scrollY / (bodyTag.scrollHeight - innerHeight);
-    return Math.floor(dec * 100);
+    return Math.floor(dec * 1000);
 }
 
 addEventListener('scroll', () => {
     console.log('Event is listening');
 
-    myNav.style.setProperty('background', scrolled()>50 ?
+    myElem.style.setProperty('background', scrolled()>50 ?
     "var(--color1)" : "var(--color2)");
 })
